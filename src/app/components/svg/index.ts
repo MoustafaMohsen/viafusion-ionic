6c5083e6437b74e74cdf494ed943c6a1933ad9f8
roqaya299 <elephantsong29@example.com>
2021-06-25 06:46:46 +0200

    dashboard changes

--- src/app/components/svg/index.ts
+++ src/app/components/svg/index.ts
@@ -2,11 +2,13 @@ import { nfcIcon } from './consts/nfc';
 import { recieveIcon } from './consts/recieve';
 import { sendIcon } from './consts/send';
 import { transferIcon } from './consts/transfer';
+import { notificationIcon } from './consts/notification';
 
 export const  icons = [
     nfcIcon,
     recieveIcon,
     sendIcon,
-    transferIcon
+    transferIcon,
+    notificationIcon,
 
 ];
