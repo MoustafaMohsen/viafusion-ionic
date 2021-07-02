014e830964974012915f0b32538afa64d2850197
roqaya299 <elephantsong29@example.com>
2021-07-02 02:06:40 +0200

    input in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input() title = "";
   constructor() { }
 
   ngOnInit() {}
   back(){
-    
+
   }
 
 }
