91c6d7108285c524370b631152fa3f76740af18f
Moustafa Mohsen <moustafamohsen1@gmail.com>
2021-07-06 11:51:36 +0200

    fixed bug

--- src/app/pages/actions/complete-transaction/complete-transaction.page.ts
+++ src/app/pages/actions/complete-transaction/complete-transaction.page.ts
@@ -18,119 +18,20 @@ export class CompleteTransactionPage implements OnInit {
 
   constructor(private rx: RX, private router: Router, private walletSrv: WalletService, public loading: LoadingService) { }
 
-  @Input() transaction: ITransaction = {
-    "payments": [
-      {
-        "request": {
-          "amount": 333,
-          "payment_method": {
-            "type": "us_mastercard_card",
-            "fields": {
-              "name": "fasf",
-              "number": "5446627313825177",
-              "expiration_month": "12",
-              "expiration_year": "25",
-              "cvv": "555"
-            }
-          },
-          "metadata": {
-            "name": "Mastercard",
-            "image": "https://iconslib.rapyd.net/checkout/us_mastercard_card.png",
-            "category": "card"
-          },
-          "3DS_requirede": false,
-          "address": {
-            "id": "address_c487057e5e227bf36ce24a459cc56b88",
-            "name": "asfds",
-            "line_1": "asdfsdaf",
-            "line_2": "",
-            "line_3": "",
-            "city": "asdf",
-            "state": "fadsf",
-            "country": "US",
-            "zip": "5500",
-            "phone_number": "",
-            "metadata": {},
-            "canton": "",
-            "district": "",
-            "created_at": 1625479611
-          },
-          "currency": "USD",
-          "complete_payment_url": "https://google.com/",
-          "error_payment_url": "https://google.com/",
-          "description": "",
-          "capture": true,
-          "customer": "cus_89358af8f82d0d61ccad7055efd22f5e",
-          "statement_descriptor": "Test Transfer",
-          "ewallets": [
-            {
-              "ewallet": "ewallet_eba088836a8e0fe6d29c8909be7cbe13",
-              "percentage": 100
-            }
-          ]
-        }
-      }
-    ],
-    "id": "tranid_bQrWd",
-    "source_amount": "333",
-    "destination_amount": "0333",
-    "payouts": [
-      {
-        "request": {
-          "payout_amount": "333",
-          "payout_method_type": "us_mastercard_card",
-          "payout_currency": "USD",
-          "beneficiary_country": "US",
-          "beneficiary_entity_type": "individual",
-          "beneficiary": {
-            "email": "ffas@ffsdf.com",
-            "company_name": "af",
-            "postcode": "165165",
-            "number": "5446627313825177",
-            "expiration_month": "12",
-            "expiration_year": "25",
-            "cvv": "555"
-          },
-          "sender": {
-            "email": "ffas@ffsdf.com",
-            "company_name": "af",
-            "postcode": "165165"
-          },
-          "sender_country": "US",
-          "sender_currency": "USD",
-          "sender_entity_type": "individual",
-          "ewallet": "ewallet_eba088836a8e0fe6d29c8909be7cbe13",
-          "merchant_reference_id": "vZjGq",
-          "metadata": {
-            "name": "null",
-            "image": "null",
-            "category": "null"
-          },
-          "description": "",
-          "confirm_automatically": true,
-          "statement_descriptor": "Test Transfer"
-        }
-      }
-    ],
-    "transfer_resoponse": {},
-    "execute": true,
-    "executed": false,
-    "type": "many2many"
-  } as any;
+  @Input() transaction: ITransaction ;
   _sub: Subscription
   ionViewWillEnter() {
-    this.transaction
-    this._sub = this.rx.temp.view_transaction.subscribe(t => {
-      this.transaction = t
-      console.log("transaction overview updated");
-      console.log(this.transaction);
-    })
   }
 
   ionViewWillLeave() {
-    this._sub.unsubscribe();
   }
   ngOnInit() {
+    this._sub = this.rx.temp.view_transaction.subscribe(t => {
+      console.log("transaction overview updated");
+      console.log(this.transaction);
+      if(!t)return;
+      this.transaction = t
+    })
   }
 
   action_status_type(status): ActionStatusesTypes {
@@ -146,6 +47,7 @@ export class CompleteTransactionPage implements OnInit {
   }
 
   payment_action(payment: ITransactionFull_payment) {
+    return;
     if (payment.response && payment.response.body) {
       this.walletSrv.complete_payment(payment.response.body.data.id).subscribe(res => {
         if (!res.success) {
@@ -173,10 +75,12 @@ export class CompleteTransactionPage implements OnInit {
 
 
   refresh_transaction(event?) {
+    // TODO: remove return
+    setTimeout(() => {
+      event && event.target.complete();
+    }, 2000);
+    return;
     if (!this.transaction || !this.transaction.id) {
-      setTimeout(() => {
-        event && event.target.complete();
-      }, 2000);
 
       return;
     }
@@ -196,7 +100,7 @@ export class CompleteTransactionPage implements OnInit {
     })
   }
 
-  btn_active(payment: ITransactionFull_payment) {
+  btn_active(payment: ITransactionFull_payment | ITransactionFull_payout) {
     if (payment?.response?.body?.data?.status) {
       return this.action_status_type(payment.response.body.data.status).btn_active && this.loading.loading
     }
@@ -211,11 +115,13 @@ export class CompleteTransactionPage implements OnInit {
   }
 
   do_payments() {
+    return;
     this.loading.start();
     this.walletSrv.do_payments(this.transaction).then(d => this.loading.stop())
   }
 
   do_payouts() {
+    return;
     this.loading.start()
     this.walletSrv.do_payouts(this.transaction).then(d => this.loading.stop())
   }
